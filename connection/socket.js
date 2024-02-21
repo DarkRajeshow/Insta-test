9f4a2406b6d30cc2d10b4deb3a7b4b706069d080
Rajesh Adeli <rajannaadeli@gmail.com>
2024-02-21 19:27:41 +0530

    First commit

---
+++ connection/socket.js
@@ -0,0 +1,29 @@
+import { Server } from "socket.io";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default function socketIo(server) {
+    const io = new Server(server, {
+        cors: {
+            origin: process.env.CLIENT_URL,
+            methods: ["GET", "POST"],
+        },
+    });
+
+    io.on('connection', (socket) => {
+        console.log("Connected " + socket.id);
+        socket.on("join_room", (userId) => {
+            socket.join(userId);
+            socket.emit("joined", `${userId} room joined successfully`)
+        });
+
+        socket.on("new_message", (messageContent) => {
+            console.log("check");
+            io.to(messageContent.receiver).emit("receive_message", messageContent);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("user with id " + socket.id + " disconnected.");
+        });
+    });
+}
